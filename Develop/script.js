bbf319260f31ae92386a782e7e401cfae77ec67f
Forever Young <dy9040@gmail.com>
2022-01-15 12:19:57 -0600

    added uppcase, lowercase, special char, num, and password length variables

--- Develop/script.js
+++ Develop/script.js
@@ -4,15 +4,37 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-
-
-
-
-
-  
-return "Your password";
+function validateLength(passwordlength) {
+  passwordlength = number(passwordlength);
+  return !(
+    Number.isNaN(passwordlength) ||
+    paswordlength < 8 ||
+    passwordlength > 128
+  );
 }
+
+function generatePassword() {
+  var promptUpperCase = window.confirm("Would you like upper case letters?");
+  var promptLowerCase = window.confirm("Would you like lower case letters?");
+  var promptSpecialChar = window.confirm("Would you like special characters?");
+  var promptNumChar = window.confirm("Would you like numbers?");
+  var passwordlength = window.confirm("How many character do you want?");
+  
+  }
+  var promptLowerCase = window.prompt("would you like lower case letters?")
+
+//1. prompt the user for the password criteria
+// 1a password length between 8 and 128 characters
+// 1b uppercase, lowercase, special characters
+// 2 validate the input 
+// 3 generate password with criteria
+// 4 display password on the page.
+
+
+
+
+
+return "Your password";
 
 // Write password to the #password input
 function writePassword() {
